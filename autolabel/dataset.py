96466ea5a5683c9368bf82b2b0e64d341ec59653
Bin Yang <ybMaschinenbau@gmail.com>
2023-03-21 11:01:42 +0100

    remove non-related codes and reformatted

--- autolabel/dataset.py
+++ autolabel/dataset.py
@@ -202,10 +202,7 @@ class SceneDataset(torch.utils.data.IterableDataset):
                 yield self._next_train()
         else:
             for i in range(self.rotations.shape[0]):
-                # log every 10 images
-                if i % 10 == 0:
-                    yield self._get_test(i)
-                # yield self._get_test(i)
+                yield self._get_test(i)
 
     def _next_train(self):
         chunks = self.batch_size // self.sample_chunk_size
@@ -269,19 +266,18 @@ class SceneDataset(torch.utils.data.IterableDataset):
 
     def _get_test(self, image_index):
         image = self.images[image_index].reshape(self.h, self.w, 3)
-        image_name = self.image_names[image_index]
         ray_o = np.broadcast_to(self.origins[image_index],
                                 (self.h, self.w, 3)).astype(np.float32)
-        ray_d, direction_norms = self._compute_direction(image_index, np.arange(
-            self.resolution))
-        direction_norms = direction_norms.reshape(self.h, self.w, 1).astype(np.float32)
+        ray_d, direction_norms = self._compute_direction(
+            image_index, np.arange(self.resolution))
+        direction_norms = direction_norms.reshape(self.h, self.w,
+                                                  1).astype(np.float32)
         ray_d = ray_d.reshape(self.h, self.w, 3).astype(np.float32)
 
         depth = (self.depths[image_index] / 1000.0).reshape(self.h, self.w)
         semantic = (self.semantics[image_index].astype(int) - 1).reshape(
             self.h, self.w)
         out = {
-            'image_name': image_name,
             'pixels': image,
             'rays_o': ray_o,
             'rays_d': ray_d,
